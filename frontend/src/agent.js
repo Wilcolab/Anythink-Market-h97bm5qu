21deb446e6fc515fe0cf78d521c993b97114a938
bO-05 <blessedbyhope05@gmail.com>
2022-11-30 15:29:36 +0700

    add New Relic to backend

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -5,7 +5,7 @@ const superagent = superagentPromise(_superagent, global.Promise);
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
-const API_ROOT = `${BACKEND_URL}/api`;
+const API_ROOT = `${BACKEND_URL}api`;
 
 const encode = encodeURIComponent;
 const responseBody = (res) => res.body;
