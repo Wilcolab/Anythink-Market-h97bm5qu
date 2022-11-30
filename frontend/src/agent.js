c45ca5a52dad4df9f4439f8bc1bd4d7e3b136280
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2022-11-30 10:53:56 +0700

    edit agent.js

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -5,8 +5,8 @@ const superagent = superagentPromise(_superagent, global.Promise);
 
 const BACKEND_URL =
   process.env.NODE_ENV !== "production"
-    ? process.env.REACT_APP_BACKEND_URL
-    : "https://api.anythink.market";
+  ? "https://api.anythink.market"
+  : process.env.REACT_APP_BACKEND_URL
 
 const API_ROOT = `${BACKEND_URL}/api`;
 
