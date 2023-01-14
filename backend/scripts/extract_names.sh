5cb1835a0d3dae38532abec807a3884e439af73b
bO-05 <blessedbyhope05@gmail.com>
2023-01-14 17:18:21 +0700

    Revert "bashing it"
    This reverts commit 9c3a4904524ca8f4bcc01c6cabbce2fbfa0d2db2.

--- backend/scripts/extract_names.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# extract_names.sh
-
-# Store the spreadsheet URL in a variable
-spreadsheet_url="https://docs.google.com/spreadsheets/d/1fYT7vLFdgEbPKCNxJ0lY3esgEPSSkRhsajhNHf_rA4I/export?format=csv"
-
-# Use the 'curl' command to download the spreadsheet content and pipe it to the 'awk' command to extract the names
-curl -L $spreadsheet_url | awk -F "," 'tolower($4) ~ /@amazon.com/ {print $3 " " $2}'
