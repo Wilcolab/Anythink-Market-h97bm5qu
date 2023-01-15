96dfe6b93850969506444a47c199e7d11b0ac346
bO-05 <blessedbyhope05@gmail.com>
2023-01-15 10:52:18 +0700

    bashing it

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -7,4 +7,6 @@ input="$1"
 
 # $(grep -i "@amazon.com" "$file" | awk -F, '{print $3, $2}' ) > output_names.txt
 
-names=$(grep -i "@amazon.com" "$file" | awk -F, '{print $3, $2}' ) > output_names.txt
+output=$(grep -i "@amazon.com" "$file" | awk -F, '{print $3, $2}' ) > output_names.txt
+
+echo "$output"
