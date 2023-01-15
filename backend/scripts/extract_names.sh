68e7f604597e1d319e19e161d62be0678451b486
bO-05 <blessedbyhope05@gmail.com>
2023-01-15 10:33:23 +0700

    bashing up

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -2,8 +2,8 @@
 
 # extract_names.sh
 
-# Store the spreadsheet URL in a variable
-spreadsheet_url="https://docs.google.com/spreadsheets/d/1fYT7vLFdgEbPKCNxJ0lY3esgEPSSkRhsajhNHf_rA4I/export?format=csv"
+# Store input in a variable
+input="$1"
 
-# Use the 'curl' command to download the spreadsheet content and pipe it to the 'awk' command to extract the names
-curl -L $spreadsheet_url | awk -F "," 'tolower($4) ~ /@amazon.com/ {print $3 " " $2}' > output_names.txt
+# Use the 'curl' command to download the input content and pipe it to the 'awk' command to extract the names
+curl -L $input | awk -F "," 'tolower($4) ~ /@amazon.com/ {print $3 " " $2}' > output_names.txt
