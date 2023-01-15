8b9189d48147ca1239dd00756cd783167652e531
bO-05 <blessedbyhope05@gmail.com>
2023-01-15 10:46:15 +0700

    bashing up

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -7,4 +7,4 @@ input="$1"
 
 # Use the 'curl' command to download the input content and pipe it to the 'awk' command to extract the names
 # curl -L $input | awk -F "," 'tolower($4) ~ /@amazon.com/ {print $3 " " $2}' > output_names.txt
-$(grep -i "@amazon.com" "$file" | awk -F, '{print $3, $2}' ) > extracted_names.txt
+$(grep -i "@amazon.com" "$file" | awk -F, '{print $3, $2}' ) > output_names.txt
